caf488581b3a6d3688114ed84981b46ee63bf489
Kavya <109163187+kavya1097@users.noreply.github.com>
2022-12-20 19:12:44 +0530

    Create maximum subarray.java

---
+++ maximum subarray.java
@@ -0,0 +1,12 @@
+int maxSubArray(int* nums, int numsSize){
+    int sum=0;
+    int maxsum=nums[0];
+    for(int i=0;i<numsSize;i++){
+        sum+=nums[i];
+        if(sum>maxsum)
+            maxsum=sum;
+        if(sum<0)
+            sum=0;
+    }
+    return maxsum;
+}
